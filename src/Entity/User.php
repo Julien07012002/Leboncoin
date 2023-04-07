fd2635f2e5abd3bf63cbe74f89ef0d7c5870c710
Julien <julientheobald8@gmail.com>
2023-04-07 09:58:07 +0200

    entité et user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,103 +1,67 @@
 <?php
 
-
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
-/**
- * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
- */
-class User implements UserInterface
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['pseudo'], message: 'There is already an account with this pseudo')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
 
-    /**
-     * @ORM\Column(type="string", length=180, unique=true)
-     */
-    private $email;
+    #[ORM\Column(length: 180)]
+    private ?string $pseudo = null;
 
-    /**
-     * @ORM\Column(type="json")
-     */
-    private $roles = [];
+    #[ORM\Column]
+    private array $roles = [];
 
-    /**
-     * @ORM\Column(type="string")
-     */
-    private $pseudo;
+    #[ORM\Column(length: 100, unique: true)]
+    private ?string $email = null;
 
-    /**
-     * @ORM\Column(type="string")
-     */
-    private $password;
-
-    /**
-     * @ORM\OneToMany(targetEntity="Annonce", mappedBy="user")
-     */
-    private $annonces;
-
-    /**
-     * @ORM\OneToMany(targetEntity="Commentary", mappedBy="user")
-     */
-    private $commentaries;
-
-    /**
-     * @ORM\OneToMany(targetEntity="Bank", mappedBy="user")
-     */
-    private $banks;
-
-    /**
-     * @ORM\OneToMany(targetEntity="Address", mappedBy="user")
-     */
-    private $addresses;
-
-    /**
-     * @ORM\OneToMany(targetEntity="Acquisition", mappedBy="user")
-     */
-    private $acquisitions;
-
-    
-    public function __construct()
-    {
-        $this->annonces = new ArrayCollection();
-        $this->commentaries = new ArrayCollection();
-        $this->banks = new ArrayCollection();
-        $this->addresses = new ArrayCollection();
-        $this->acquisitions = new ArrayCollection();
-    }
-
-    // Getters and setters...
+    #[ORM\Column(length: 150)]
+    private ?string $password = null;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getEmail(): ?string
+    public function getPseudo(): ?string
     {
-        return $this->email;
+        return $this->pseudo;
     }
 
-    public function setEmail(string $email): self
+    public function setPseudo(string $pseudo): self
     {
-        $this->email = $email;
+        $this->pseudo = $pseudo;
 
         return $this;
     }
 
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->pseudo;
+    }
+
+    /**
+     * @see UserInterface
+     */
     public function getRoles(): array
     {
         $roles = $this->roles;
-
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
 
@@ -111,14 +75,23 @@ class User implements UserInterface
         return $this;
     }
 
-    public function getPseudo(): ?string
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
     {
-        return $this->pseudo;
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 
-    public function setPseudo(string $pseudo): self
+    public function getEmail(): ?string
     {
-        $this->pseudo = $pseudo;
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
 
         return $this;
     }
@@ -134,161 +107,4 @@ class User implements UserInterface
 
         return $this;
     }
-
-    public function getSalt()
-    {
-        // not needed when using the "bcrypt" algorithm in security.yaml
-    }
-
-    public function getUsername()
-    {
-        return $this->email;
-    }
-
-    public function eraseCredentials()
-    {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
-    }
-
-    public function getAnnonces(): Collection
-    {
-        return $this->annonces;
-    }
-
-    public function addAnnonce(Annonce $annonce): self
-    {
-        if (!$this->annonces->contains($annonce)) {
-            $this->annonces[] = $annonce;
-            $annonce->setUser($this);
-        }
-
-        return $this; 
-    }
-
-    public function removeAnnonce(Annonce $annonce): self
-    {
-        if ($this->annonces->contains($annonce)) {
-            $this->annonces->removeElement($annonce);
-            // set the owning side to null (unless already changed)
-            if ($annonce->getUser() === $this) {
-                $annonce->setUser(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function getCommentaries(): Collection
-    {
-        return $this->commentaries;
-    }
-
-    public function addCommentary(Commentary $commentary): self
-    {
-        if (!$this->commentaries->contains($commentary)) {
-            $this->commentaries[] = $commentary;
-            $commentary->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeCommentary(Commentary $commentary): self
-    {
-        if ($this->commentaries->contains($commentary)) {
-            $this->commentaries->removeElement($commentary);
-            // set the owning side to null (unless already changed)
-            if ($commentary->getUser() === $this) {
-                $commentary->setUser(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function getBanks(): Collection
-    {
-        return $this->banks;
-    }
-
-    public function addBank(Bank $bank): self
-    {
-        if (!$this->banks->contains($bank)) {
-            $this->banks[] = $bank;
-            $bank->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeBank(Bank $bank): self
-    {
-        if ($this->banks->contains($bank)) {
-            $this->banks->removeElement($bank);
-            // set the owning side to null (unless already changed)
-            if ($bank->getUser() === $this) {
-                $bank->setUser(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function getAddresses(): Collection
-    {
-        return $this->addresses;
-    }
-
-    public function addAddress(Address $address): self
-    {
-        if (!$this->addresses->contains($address)) {
-            $this->addresses[] = $address;
-            $address->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeAddress(Address $address): self
-    {
-        if ($this->addresses->contains($address)) {
-            $this->addresses->removeElement($address);
-            // set the owning side to null (unless already changed)
-            if ($address->getUser() === $this) {
-                $address->setUser(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function getAcquisitions(): Collection
-    {
-        return $this->acquisitions;
-    }
-
-    public function addAcquisition(Acquisition $acquisition): self
-    {
-        if (!$this->acquisitions->contains($acquisition)) {
-            $this->acquisitions[] = $acquisition;
-            $acquisition->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeAcquisition(Acquisition $acquisition): self
-    {
-        if ($this->acquisitions->contains($acquisition)) {
-            $this->acquisitions->removeElement($acquisition);
-            // set the owning side to null (unless already changed)
-            if ($acquisition->getUser() === $this) {
-                $acquisition->setUser(null);
-            }
-        }
-
-        return $this;
-    }
-
 }
